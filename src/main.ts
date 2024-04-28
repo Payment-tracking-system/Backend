6462825ede65980a0674ef8c7c81d9c2556845ac
zyabl1k <kdaniild@inbox.ru>
2024-04-28 10:43:10 +0500

    [UPDATE] - https

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './modules/app/app.module';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/cert.key'),
+  cert: fs.readFileSync('./secrets/cert.crt'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   const configService = app.get(ConfigService);
   const PORT = configService.get('port') || 5000;
   app.useGlobalPipes(new ValidationPipe());
